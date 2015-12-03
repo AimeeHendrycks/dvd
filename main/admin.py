8d7cef56a0bc3f2689a4f90b4d2789fb4fc38e36
AimeeH <am.hendrycks@gmail.com>
2015-12-03 09:48:28 -0700

    admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from main.models import Dvd
+from main.models import Dvd, Genre, Studio
 # Register your models here.
 class DvdAdmin(admin.ModelAdmin):
     search_fields =['title']
     list_display = ('title', 'studio')
+
+class GenreAdmin(admin.ModelAdmin):
+    search_fields =['genre']
+
+class StudioAdmin(admin.ModelAdmin):
+    search_fields =['studio']
 admin.site.register(Dvd, DvdAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Studio, StudioAdmin)
