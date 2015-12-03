6d6b85c9a39afb4eb8b99fdc6a4b95644b216faa
AimeeH <am.hendrycks@gmail.com>
2015-12-02 18:02:33 -0700

    urls

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-import debug_toolbar
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^dvd_list/$', 'main.views.dvd_list'),
