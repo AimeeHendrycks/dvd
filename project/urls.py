10ec8aaac2e3292144b047a050fdd16b33d5d874
AimeeH <am.hendrycks@gmail.com>
2015-12-17 10:20:39 -0700

    switching urls around

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^dvd_detail/(?P<pk>\d+)/$', 'main.views.dvd_detail'),
     url(r'^dvd_list_mysql/$', 'main.views.dvd_list_mysql'),
     url(r'^dvd_detail_mysql/(?P<pk>\d+)/$', 'main.views.dvd_detail_mysql'),
-    url(r'^$', 'main.views.home'),
-    url(r'^index/$', 'main.views.index'),
+    url(r'^$', 'main.views.index'),
+    url(r'^api/$', 'main.views.home'),
 
 ]
